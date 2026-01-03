5d8ad2dd1a6524ad3b4b3eea3b09dcb7dea2bb61
sebmaz <seb.maz93@icloud.com>
2026-01-04 00:15:17 +0400

    add raindrops solution

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,5 +1,20 @@
 package raindrops
 
+import "strconv"
+
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	text := ""
+	if number%3 == 0 {
+		text += "Pling"
+	}
+	if number%5 == 0 {
+		text += "Plang"
+	}
+	if number%7 == 0 {
+		text += "Plong"
+	}
+	if text == "" {
+		return strconv.Itoa(number)
+	}
+	return text
 }
