e377db4f339f6b0e2379d286f45c1777de700d83
sebmaz <seb.maz93@icloud.com>
2026-01-05 02:14:42 +0400

    add prime factors solution

--- prime-factors/prime_factors.go
+++ prime-factors/prime_factors.go
@@ -1,5 +1,22 @@
 package prime
 
 func Factors(n int64) []int64 {
-	panic("Please implement the Factors function")
+	fac := []int64{}
+	num := n
+
+	if n == 1 {
+		return []int64{1}
+	}
+
+	divisor := int64(2)
+
+	for num > 1 {
+		if num%divisor == 0 {
+			fac = append(fac, divisor)
+			num = num / divisor
+		} else {
+			divisor++
+		}
+	}
+	return fac
 }
